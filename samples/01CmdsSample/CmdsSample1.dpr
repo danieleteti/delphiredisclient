b76c53fe2793982d324ba03c9287e49b44a93b99
danieleteti <d.teti@bittime.it>
2014-07-08 12:43:34 +0200

    Command Sample 1

---
+++ samples/01CmdsSample/CmdsSample1.dpr
@@ -0,0 +1,33 @@
+program CmdsSample1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+
+uses
+  System.SysUtils, Redis.Client, Redis.NetLib.INDY;
+
+var
+  Redis: IRedisClient;
+  Value: string;
+
+begin
+  try
+    Redis := NewRedisClient('localhost');
+    Redis.&SET('firstname', 'Daniele');
+    Redis.GET('firstname', Value);
+    WriteLn('key firstname, value ', Value);
+    WriteLn('DEL firstname');
+    Redis.DEL(['firstname']);
+    if Redis.GET('firstname', Value) then
+      write(Value)
+    else
+      write('Key firstname not exists');
+  except
+    on E: Exception do
+      WriteLn(E.ClassName, ': ', E.Message);
+  end;
+  readln;
+
+end.
