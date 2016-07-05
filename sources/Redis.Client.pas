cd2937610f0b14f855b4b17d5b51f145aec0200e
Bob Florian <bobflorian@users.noreply.github.com>
2016-07-05 14:46:19 -0500

    Update Redis.Client.pas
    Update reference in HMGET from NextCMD to Pieces variable.  NextCMD is nil at runtime in this command.

--- sources/Redis.Client.pas
+++ sources/Redis.Client.pas
@@ -404,7 +404,7 @@ begin
   Pieces.Add(AKey);
   for I := Low(aFields) to High(aFields) do
   begin
-    NextCMD.Add(aFields[I]);
+    Pieces.Add(aFields[I]);
   end;
   FTCPLibInstance.SendCmd(Pieces);
   Result := ParseArrayResponse(FValidResponse)
